c67b0aef17107d53710c80ca26a7e0072028d3e7
George Mejia <gmejia.fisi@gmail.com>
2012-06-10 05:15:26 -0500

    Construccion de la estructura base de 3h

---
+++ 3hweb/src/main/java/org/fisi/core/Entidad.java
@@ -0,0 +1,20 @@
+package org.fisi.core;
+
+/**
+ * 
+ * @author GEORGE MEJIA
+ *
+ */
+public class Entidad {
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
